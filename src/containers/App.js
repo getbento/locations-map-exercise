e841ccb9678a09ffc07350ebc025d0a4c1b67af4
Bobby Lux <robert.michael.lux@gmail.com>
2019-03-30 16:00:14 -0400

    feat: add markers and popups at restaurant locations

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 
 import { fetchLocations } from '../helpers/api';
 
@@ -10,48 +12,90 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 1.5,
-      locations: null
+      loading: true,
+      lng: -100,
+      lat: 39,
+      zoom: 2.5,
+      selectedLocation: null
     };
   }
 
   componentDidMount() {
     const { lng, lat, zoom } = this.state;
 
-    const map = new mapboxgl.Map({
+    this.tooltipContainer = document.createElement('div');
+
+    this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
       center: [lng, lat],
       zoom
     });
 
-    map.on('move', () => {
-      const { lng, lat } = map.getCenter();
+    const tooltip = new mapboxgl.Marker(this.tooltipContainer, {
+      offset: [-120, 0]
+    }).setLngLat([0, 0]).addTo(this.map);
+
+    this.map.on('move', () => {
+      const { lng, lat } = this.map.getCenter();
 
       this.setState({
         lng: lng.toFixed(4),
         lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+        zoom: this.map.getZoom().toFixed(2)
       });
     });
+
     fetchLocations()
-      .then(response => this.setState({ locations: response }))
+      .then(locations => this.fetchCoordinatesAndAddToLocationObjects(locations))
+      .then(locationsWithMapboxFeatures => this.createMarkers(locationsWithMapboxFeatures))
       .catch(error => console.log(error));
   }
 
-  async getCoordinatesFromAddresses() {
-    const { locations } = this.state;
-    const promisesOfLocationsWithCoordinates = locations.map(locaiton => {
+  // setTooltip(location) {
+  //   ReactDOM.render(
+  //     React.createElement(
+  //       Tooltip, {
+  //         features
+  //       }
+  //     ),
+  //     this.tooltipContainer
+  //   );
+  // }
 
-    })
+  createMarkers(locationsWithFeatues) {
+    locationsWithFeatues.forEach(locationWithFeature => {
+      const popupEl = document.createElement('div');
+      popupEl.innerHTML = 'Hello World';
+      const popup = new mapboxgl.Popup()
+        .setDOMContent(popupEl);
+      const el = document.createElement('div');
+      // el.addEventListener('click', () => this.setTooltip(locationWithFeature));
+      el.className = 'marker';
+      new mapboxgl.Marker(el)
+        .setPopup(popup)
+        .setLngLat(locationWithFeature.mapboxFeature[0].center)
+        .addTo(this.map);
+    });
+  }
+
+  async fetchCoordinatesAndAddToLocationObjects(locations) {
+    const promisesOfLocationsWithMapboxFeatures = locations.map(async location => {
+      const mapboxApiResponse = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location.address}.json?limit=1&&types=address&&access_token=${mapboxgl.accessToken}`);
+      return { locationData: location, mapboxFeature: mapboxApiResponse.data.features };
+    });
+    const locationsWithMapboxFeatures = await Promise.all(promisesOfLocationsWithMapboxFeatures);
+    return locationsWithMapboxFeatures;
   }
 
   render() {
-    console.log('this.state:', this.state)
+    console.log('this.state:', this.state);
     return (
       <div style={{ width: '100vw', height: '100vh', margin: '0px', padding: '0px' }}>
+        {this.state.loading
+          ? <h1 style={{ position: 'absolute', zIndex: '100' }}>Loading...</h1>
+          : null
+        }
         <div ref={el => { this.mapContainer = el; }} style={{ width: '100vw', height: '100vh', margin: '0px', padding: '0px' }} />
       </div>
     );
