3a802afd3b2b92b15fd1c47ca0aa031b5a0c8aa6
Bobby Lux <robert.michael.lux@gmail.com>
2019-03-30 11:59:04 -0400

    chore: add fetched locations to local state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import mapboxgl from 'mapbox-gl';
 
 import { fetchLocations } from '../helpers/api';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJlc2hicmVhZGx1eCIsImEiOiJjanR2bGp4c24xeWd3NDVuc2RtZXUyY204In0.QCOQD8hUh17MTB4KX3nPpA';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1.5,
+      locations: null
+    };
+  }
+
   componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
     fetchLocations()
-      .then(response => console.log(response))
+      .then(response => this.setState({ locations: response }))
       .catch(error => console.log(error));
   }
 
+  async getCoordinatesFromAddresses() {
+    const { locations } = this.state;
+    const promisesOfLocationsWithCoordinates = locations.map(locaiton => {
+
+    })
+  }
+
   render() {
-    return <h1>Locations Map Exercise!</h1>;
+    console.log('this.state:', this.state)
+    return (
+      <div style={{ width: '100vw', height: '100vh', margin: '0px', padding: '0px' }}>
+        <div ref={el => { this.mapContainer = el; }} style={{ width: '100vw', height: '100vh', margin: '0px', padding: '0px' }} />
+      </div>
+    );
   }
 }
 
