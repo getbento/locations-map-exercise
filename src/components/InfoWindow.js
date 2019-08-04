9e7994ca5cca44c699ab0d881b759829e1b8004f
Ronit Kory <ronitronitronit@gmail.com>
2019-08-04 15:33:37 -0400

    basic info windows for markers

---
+++ src/components/InfoWindow.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const InfoWindowWrapper = styled.div`
+  & * {
+    margin: 0;
+  }
+`;
+const DetailsLink = styled.p`
+  font-weight: 300;
+  font-style: italic;
+  cursor: pointer;
+  color: blue;
+`;
+
+const InfoWindow = props => {
+  const item = props.item;
+  console.log(item);
+  return (
+    <InfoWindowWrapper>
+      <h4>{item.name}</h4>
+      <p>{item.address}</p>
+      <DetailsLink onClick={props.showExtendedInfo}>
+        {"View Details"}
+      </DetailsLink>
+    </InfoWindowWrapper>
+  );
+};
+
+export default InfoWindow;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,21 @@
 import React, { createRef } from "react";
 import styled from "styled-components";
+import InfoWindow from './InfoWindow'
+import {render} from 'react-dom';
 
 const MapBox = styled.div`
   width: 800px;
   height: 600px;
-  z-index: -1;
   position: relative;
 `;
 
-const MapBoxWrapper = styled.div`
-
-`
+const MapBoxWrapper = styled.div``;
 
 const Overlay = styled.div`
   z-index: 1000;
   height: 600px;
   width: 800px;
-  background: rgba(10,10,10,0.5);
+  background: rgba(10, 10, 10, 0.5);
   position: absolute;
   display: flex;
   align-items: center;
@@ -31,7 +30,8 @@ const Overlay = styled.div`
 class Map extends React.Component {
   state = {
     loading: true,
-    markerCount: 0
+    markerCount: 0,
+    currentItem: 'bob'
   };
 
   componentDidMount() {
@@ -40,13 +40,13 @@ class Map extends React.Component {
       googleMapsScript.src = `https://maps.google.com/maps/api/js?key=AIzaSyB9iEsdx1njWPrzPRLSQTF5TbCdWoQa0SM`;
       document.body.appendChild(googleMapsScript);
       googleMapsScript.addEventListener("load", e => {
-        this.inializeMap();
+        this.intializeMap();
       });
     } else {
-      this.inializeMap();
+      this.intializeMap();
     }
   }
-  inializeMap = () => {
+  intializeMap = () => {
     this.setState({
       loading: false
     });
@@ -61,7 +61,10 @@ class Map extends React.Component {
   // per minute. Instead of waiting a minute, here I call each one at an
   // interval of 500 milliseconds which seems to work smoothly
   createMarkers = (addressData, resultsMap) => {
-    let number = 0;
+    // there's some scope(?) issue inside of the Google functions where it
+    // does not recognize the methods of the current React class as valid. The below
+    // line looks weird but it seems to remedy it  ¯\_(ツ)_/¯
+    let createInfoWindow = this.createInfoWindow;
     const geocoder = new google.maps.Geocoder();
     addressData.map(item => {
       setTimeout(() => {
@@ -70,9 +73,13 @@ class Map extends React.Component {
         });
         geocoder.geocode({ address: item.address }, function(results, status) {
           if (status == "OK") {
-            new google.maps.Marker({
+            const marker = new google.maps.Marker({
               map: resultsMap,
-              position: results[0].geometry.location
+              position: results[0].geometry.location,
+              title: item.name
+            });
+            marker.addListener("click", e => {
+              createInfoWindow(e, resultsMap, item);
             });
           } else {
             console.error(
@@ -83,6 +90,21 @@ class Map extends React.Component {
       }, addressData.indexOf(item) * 500);
     });
   };
+// this comes basically straight out of here https://cuneyt.aliustaoglu.biz/en/using-google-maps-in-react-without-custom-libraries/
+  createInfoWindow = (e, resultsMap, item) => {
+    const infoWindow = new window.google.maps.InfoWindow({
+      content: '<div id="infoWindow" />',
+      position: { lat: e.latLng.lat(), lng: e.latLng.lng() }
+    });
+    infoWindow.addListener('domready', e => {
+      render(<InfoWindow item={item} showExtendedInfo={this.showExtendedInfo}/>, document.getElementById('infoWindow'))
+    })
+    infoWindow.open(resultsMap)
+  };
+
+  showExtendedInfo = (item) => {
+  console.log('got to show extended info', item)
+}
 
   mapBoxRef = createRef();
   render() {
@@ -93,7 +115,9 @@ class Map extends React.Component {
         ) : (
           <MapBoxWrapper>
             {this.state.markerCount !== this.props.locations.length && (
-              <Overlay><h1>Loading locations...</h1></Overlay>
+              <Overlay>
+                <h1>Loading locations...</h1>
+              </Overlay>
             )}
             <MapBox ref={this.mapBoxRef} />
           </MapBoxWrapper>
